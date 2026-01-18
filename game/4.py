b322e2987ed3af723304fc095450c1a62308f621
anksingh90 <ankitsingh90@duck.com>
2026-01-18 06:31:05 +0000

    update

--- game/4.py
+++ game/4.py
@@ -13,32 +13,34 @@ print("press 3 to exit")
 
 ch=int(input("Enter your choice :"))
 points=[]
-guess=[]
+guess=[3, 3, 3, 3, 5]
 if ch==1:
     print("Rolling 5 dice..")
+    '''
     for i in range(5):
         value=random.randint(1,6)
         guess.append(value)
-    
+    '''
     # printing the dice list
     '''
     Position:  0   1   2   3   4
     Dice:     [2] [4] [2] [6] [2]
     '''
     print(guess)
-
+#guess[0]==guess[1] or guess[1]!=guess[2] or guess[2]!=guess[3] or guess[3]!=guess[4] or guess[4]!=guess[0]:
     # compare or check if any index is mataching !
-
-    if guess[0]==guess[1] or guess[1]==guess[2] or guess[2]==guess[3] or guess[3]==guess[4] or guess[4]==guess[0]:
-        print(" JACKPOT! FIVE OF A KIND!")
-        print("points :",500)
-        print("do you want to Re-rolls your dice","yes" or "no")
-    elif guess[0]!=guess[1] or guess[1]!=guess[2] or guess[2]!=guess[3] or guess[3]!=guess[4] or guess[4]!=guess[0]:  
-        print("points :",150)
+#print("do you want to Re-rolls your dice : y/n ")
+    if guess[0]==guess[1] or guess[1]==guess[2] or guess[2]==guess[3] or guess[3]==guess[4] or guess[4]==guess[0]: 
+       print(" JACKPOT! FIVE OF A KIND!")
+       print("points :",500)
+        
+    elif guess[0]==guess[1] or guess[0]!=guess[1]  or guess[1]==guess[2] or guess[1]!=guess[2]or guess[2]==guess[3] or guess[2]!=guess[3] or guess[3]==guess[4]  or guess[3]!=guess[4] or guess[4]==guess[0] or guess[4]!=guess[0] :
+        print("points :",400)
         print("do you want to re-rolls your dice","yes" or "no")
-        pos=int(input("Enter position you want to re-rolls "))
-        print("Re-rolls of dice",pos)
-        guess1=random.randint(1,6)
+        num=int(input("Enter number of position you want to re-rolls "))
+        for i in num:
+             pos=int(input("enter position you want to re-rolls"))
+             guess1=random.randint(1,6)
         print("position",pos,"dice :",guess1)
     elif guess[0]!=guess[1]!=guess[2] or guess[2]!=guess[3]!=guess[4]:
         print("points :",400)
