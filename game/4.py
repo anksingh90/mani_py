11ebd6b8881f2447ed405855b2124f2627e9fee3
anksingh90 <ankitsingh90@duck.com>
2026-01-18 07:22:16 +0000

    update

--- game/4.py
+++ game/4.py
@@ -13,35 +13,20 @@ print("press 3 to exit")
 
 ch=int(input("Enter your choice :"))
 points=0
-guess=[3, 3, 3, 3, 5]
+guess=random.randint(1,6)
 if ch==1:
-    print("Rolling 5 dice..")
-    '''
-    for i in range(5):
-        value=random.randint(1,6)
-        guess.append(value)
-    '''
-    # printing the dice list
-    '''
-    Position:  0   1   2   3   4
-    Dice:     [2] [4] [2] [6] [2]
-    '''
-    print(guess)
-#guess[0]==guess[1] or guess[1]!=guess[2] or guess[2]!=guess[3] or guess[3]!=guess[4] or guess[4]!=guess[0]:
-    # compare or check if any index is mataching !
-#print("do you want to Re-rolls your dice : y/n ")
+    count={}
+    for value in guess:
+        count[value]=count.get(value,0)+1
     if set(guess)=={1} or set(guess)=={2} or set(guess)=={3} or set(guess)=={4} or set(guess)=={5} or set(guess)=={6}:
        print("current Hand:,Five of a Kind")
-       print("potential Score:,)
-
-    
-
-       print("points :",500)
+       points+=500
+       print("potential Score:",500)
     elif guess[0]==guess[1] or guess[0]!=guess[1]  or guess[1]==guess[2] or guess[1]!=guess[2]or guess[2]==guess[3] or guess[2]!=guess[3] or guess[3]==guess[4]  or guess[3]!=guess[4] or guess[4]==guess[0] or guess[4]!=guess[0] :
         print("points :",400)
         print("do you want to re-rolls your dice","yes" or "no")
         num=int(input("Enter number of position you want to re-rolls "))
-        for i in num:
+        for i in range(num):
              pos=int(input("enter position you want to re-rolls"))
              guess1=random.randint(1,6)
         print("position",pos,"dice :",guess1)
