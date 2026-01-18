057a092691daecb3bc1188c27656643da802e04a
anksingh90 <ankitsingh90@duck.com>
2026-01-18 07:47:01 +0000

    update

--- game/4.py
+++ game/4.py
@@ -1,11 +1,5 @@
 import random
 
-#Hand Distribution:
-#Full House: 2 (40%)
-#Straight: 1 (20%)
-#Three of a Kind: 1 (20%)
-#One Pair: 1 (20%)
-
 print("**!!Welcome to Poker dice game!!**")
 print("press 1 to play single round mode ")
 print("press 2 to play multi round mode")
@@ -13,23 +7,29 @@ print("press 3 to exit")
 
 ch=int(input("Enter your choice :"))
 points=0
-guess=random.randint(1,6)
+guess=[3, 3, 3, 3, 5]
+
+for i in range(0,5):
+    value=random.randint(1,6)
+    guess.append(value)
+print(guess)
+
 if ch==1:
     count={}
     for value in guess:
         count[value]=count.get(value,0)+1
+    
     if set(guess)=={1} or set(guess)=={2} or set(guess)=={3} or set(guess)=={4} or set(guess)=={5} or set(guess)=={6}:
        print("current Hand:,Five of a Kind")
        points+=500
-       print("potential Score:",500)
-    elif guess[0]==guess[1] or guess[0]!=guess[1]  or guess[1]==guess[2] or guess[1]!=guess[2]or guess[2]==guess[3] or guess[2]!=guess[3] or guess[3]==guess[4]  or guess[3]!=guess[4] or guess[4]==guess[0] or guess[4]!=guess[0] :
-        print("points :",400)
-        print("do you want to re-rolls your dice","yes" or "no")
-        num=int(input("Enter number of position you want to re-rolls "))
-        for i in range(num):
-             pos=int(input("enter position you want to re-rolls"))
-             guess1=random.randint(1,6)
-        print("position",pos,"dice :",guess1)
+       print("potential Score:",points)
+    
+    elif 4 in count.values():
+       print("current Hand:,Five of a Kind")
+       points+=400
+       print("potential Score:",points)
+       
+
     elif guess[0]==guess[1]==guess[2] or guess[2]==guess[3]==guess[4]:
         print("points :",400)
         print("do you want to re-rolls your dice","yes" or "no")
