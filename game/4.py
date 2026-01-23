da57b3e31129c1f0299e054451d3a48f2269a057
anksingh90 <ankitsingh90@duck.com>
2026-01-23 09:14:15 +0000

    update

--- game/4.py
+++ game/4.py
@@ -12,7 +12,6 @@ for i in range(0,5):
     value=random.randint(1,6)
     guess.append(value)
 print(guess)
-
 while True:
     round = 1 # checks the round of loop
 
@@ -35,25 +34,37 @@ while True:
         print("current Hand :,full house")
         points+=300
         print("potential score:",points) 
-        break # remove this break
+        
     elif 3 in count.values():
         print("current Hand :,Three of a kind")
         points+=150
         print("potential score:",points) 
-        break # remove this break
+        
     elif 2 in count.values() and 2 in count.values():
         print("current Hand :,two pair")
         points+=100
         print("potential score:",points) 
-        break # remove this break
+        
     elif 1 in count.values():
         print("current Hand :,one pair")
         points+=50
         print("potential score:",points) 
-        break # remove this break
-    '''
+    print("Re-rolls remaining :",round-3)
+    print("**!!**")
+
+    ch=input("Keep this hand or try to improve :").lower()
+    if ch=='n':
+        num=int(input("enter number of positions to be re-rolls :"))
+        for i in range (num):
+            pos=int(input(" Enter positions to be re-rolls :"))
+            guess1=random.randint(1,6)
+            guess[pos]=guess1
+        print(guess)
+    else:
+        print("want to keep this hand")
+        break
+
     if round == 3:
         break       
     else:
         round = round + 1
-    '''
